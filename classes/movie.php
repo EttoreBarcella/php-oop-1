53b1fa20dbad40a2c377a31527000bc91c57a20e
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-06-27 15:47:11 +0200

    added class movie + method

---
+++ classes/movie.php
@@ -0,0 +1,25 @@
+<?php
+
+class Movie{
+    public $title;
+    public $movieDirector;
+    public $year;
+    public $duration;
+    public $price;
+
+    public $discount = 0;
+
+    public function __construct($_title, $_movieDirector, $_year, $_duration, $_price){
+
+        $this->title = $_title;
+        $this->movieDirector = $_movieDirector;
+        $this->year = $_year;
+        $this->duration = $_duration;
+        $this->price = $_price;
+
+        if($this->year < 2020) {
+            $this->discount = $this->price * 0.8;
+    }
+
+
+}
